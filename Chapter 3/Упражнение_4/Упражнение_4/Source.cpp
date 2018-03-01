5ed50d1c27769886c5c3f6cd85ec9b36470af455
nebiros_88@mail.ru <nebiros_88@mail.ru>
2018-03-01 15:30:30 +0300

    MinMax

--- Chapter 3/Упражнение_4/Упражнение_4/Source.cpp
+++ Chapter 3/Упражнение_4/Упражнение_4/Source.cpp
@@ -5,6 +5,16 @@ int main()
 	int val1;
 	int val2;
 	cin >> val1 >> val2;
+	if (val1 < val2) {
+		int min = val1;
+		int max = val2;
+		cout << "Min = " << min << "Max = " << max << '\n';
+	}
+	if (val1 > val2) {
+		int min = val2;
+		int max = val1;
+		cout << "Max = " << max << "Min = " << min << '\n';
+	}
 	cout << "\nSumm of values = " << val1 + val2 << " \n";
 	cout << "\nDifference of values = " << val1 - val2 << " \n";
 	cout << "\nComposition of values = " << val1 * val2 << " \n";
