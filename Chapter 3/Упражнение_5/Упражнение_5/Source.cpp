48a42241b7aae4fee06ee0dc63d50e05d27c2d2c
nebiros_88@mail.ru <nebiros_88@mail.ru>
2018-03-02 09:12:31 +0300

    WHY no min and max?

---
+++ Chapter 3/Упражнение_5/Упражнение_5/Source.cpp
@@ -0,0 +1,22 @@
+#include "../../../std_lib_facilities.h"
+int main()
+{
+	double val1;
+	double val2;
+	cout << "Press any two values \n";
+	cin >> val1 >> val2;
+	double min = val1;
+	double max = val2;
+	double x = val1;
+	double y = val2;
+	if (val1 > val2) {
+		min = val2;
+		max = val1;
+		cout << "Min = " << min << "Max = " << max << '\n';
+	}
+	cout << "\nSumm of values = " << val1 + val2 << " \n";
+	cout << "\nDifference of values = " << val1 - val2 << " \n";
+	cout << "\nComposition of values = " << val1 * val2 << " \n";
+	cout << "\nQuotient of values = " << x / y << " \n";
+	keep_window_open();
+}
