b8be170ee82363b45198dda0e1c93d7be33963da
nebiros_88@mail.ru <nebiros_88@mail.ru>
2018-03-05 16:04:12 +0300

    Even or not even

---
+++ Chapter 3/Task_8/Task_8/source.cpp
@@ -0,0 +1,15 @@
+#include "../../../std_lib_facilities.h"
+int main()
+{
+	int a;
+	cout << "Please, enter any number and press 'Enter':\n";
+	cin >> a;
+		if (a % 2 > 0 || a % 2 < 1) {
+			cout << "Number " << a << " is not even.\n";
+		}
+		if (a % 2 = 0 || a % 2 >= 1) {
+			cout << "Number " << a << " is even.\n";
+		}
+		keep_window_open();
+		return 0;
+}
