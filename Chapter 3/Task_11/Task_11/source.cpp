8a9378cacd19ac177a52dbdc93c64840e9b31237
nebiros_88@mail.ru <nebiros_88@mail.ru>
2018-03-21 08:27:59 +0300

    if

--- Chapter 3/Task_11/Task_11/source.cpp
+++ Chapter 3/Task_11/Task_11/source.cpp
@@ -26,15 +26,25 @@ int main()
 		cout << "You have only " << a1 << " one-cent monet.\n";
 	else if (a1 > 0)
 		cout << "You have " << a1 << " one-cent monets.\n";
-	if (a5 > 0)
+	if (a5 == 1)
+		cout << "You have only " << a5 << " five-cent monet.\n";
+	else if (a5 > 0)
 		cout << "You have " << a5 << " five-cent monets.\n";
-	if (a10 > 0)
+	if (a10 == 1)
+		cout << "You have only " << a10 << " ten-cent monet.\n";
+	else if (a10 > 0)
 		cout << "You have " << a10 << " ten-cent monets.\n";
-	if (a25 > 0)
+	if (a25 == 1)
+		cout << "You have only " << a25 << " twenty-five-cent monet.\n";
+	else if (a25 > 0)
 		cout << "You have " << a25 << " twenty-five-cent monets.\n";
-	if (a50 > 0)
+	if (a50 == 1)
+		cout << "You have only " << a50 << " fifty-cent monet.\n";
+	else if (a50 > 0)
 		cout << "You have " << a50 << " fifty-cent monets.\n";
-	if (a100 > 0)
+	if (a100 == 1)
+		cout << "You have only " << a100 << " one-dollar monet.\n";
+	else if (a100 > 0)
 		cout << "you have " << a100 << " one-dollar monets.\n";
 	summ = a1 * 1 + a5 * 5 + a10 * 10 + a25 * 25 + a50 * 50 + a100 * 100;
 	dollar = summ / 100;
