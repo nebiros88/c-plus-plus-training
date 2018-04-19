cddef630554024373dc110d064ed6da55d22deb9
nebiros_88@mail.ru <nebiros_88@mail.ru>
2018-04-19 10:51:23 +0300

    ***

--- Chapter 4/Tasks/Task_1-11/Task_1-11/source.cpp
+++ Chapter 4/Tasks/Task_1-11/Task_1-11/source.cpp
@@ -9,6 +9,9 @@ bool units(string unit) {
 }
 int main()
 {
+	constexpr double cm_per_meter = 100;
+	constexpr double cm_per_inches = 2.54;
+	constexpr double in_per_feets = 12;
 	double a = 0;
 	double min = 0;
 	double max = 0;
@@ -16,23 +19,24 @@ int main()
 	string unit;
 	cout << "Please, type any value of distance and unit and press 'Enter'.\n";
 	while (cin >> a >> unit) {
-		if (units(unit))
+		if (units(unit)) {
 			++i;
-		else cout << "Choose another correct unit.\n";
-		if (i == 1 && units(unit)) {
-			min = a;
-			max = a;
-			cout << min << "\t minimal value.\n";
-			cout << max << "\t maximal value.\n";
+			if (i == 1) {
+				min = a;
+				max = a;
+				cout << min << "\t minimal value.\n";
+				cout << max << "\t maximal value.\n";
+			}
+			else if (a < min) {
+				min = a;
+				cout << min << "\t Minimal value.\n";
+			}
+			else if (a > max) {
+				max = a;
+				cout << max << "\t Maximal value.\n";
+			}
 		}
-		else if (a < min && units(unit)) {
-			min = a;
-			cout << min << "\t minimal value.\n";
-		}
-		else if (a > max && units(unit)) {
-			max = a;
-			cout << max << "\t maximal value.\n";
-		}
+		else cout << "Not correct!\n";
 	}
 	keep_window_open();
 }
