e1c1e52d4e18e21c38bf78f3bb7e95875e2bfd23
nebiros_88@mail.ru <nebiros_88@mail.ru>
2018-04-19 16:01:25 +0300

    last

--- Chapter 4/Tasks/Task_1-11/Task_1-11/source.cpp
+++ Chapter 4/Tasks/Task_1-11/Task_1-11/source.cpp
@@ -1,4 +1,20 @@
 #include "../../../../std_lib_facilities.h"
+double convert(double a, string unit) {
+	constexpr double cm_per_meter = 100;
+	constexpr double cm_per_inches = 2.54;
+	constexpr double in_per_feets = 12;
+	double result = a;
+	if (unit == "cm") {
+		result = a / cm_per_meter;
+	}
+	else if (unit == "in") {
+		 result = a * cm_per_inches / cm_per_meter;
+	}
+	else if (unit == "ft") {
+		result = a / in_per_feets * cm_per_inches / cm_per_meter;
+	}
+	return result;						//возвращаем в метрах
+}
 bool units(string unit) {
 	vector<string> units = { "cm", "in", "ft", "m" };
 	for (int i = 0; i < units.size(); ++i)
@@ -9,34 +25,34 @@ bool units(string unit) {
 }
 int main()
 {
-	constexpr double cm_per_meter = 100;
-	constexpr double cm_per_inches = 2.54;
-	constexpr double in_per_feets = 12;
 	double a = 0;
 	double min = 0;
 	double max = 0;
 	int i = 0;
 	string unit;
+	double summ = 0;
 	cout << "Please, type any value of distance and unit and press 'Enter'.\n";
 	while (cin >> a >> unit) {
+		double val = convert(a, unit);
 		if (units(unit)) {
 			++i;
 			if (i == 1) {
-				min = a;
-				max = a;
-				cout << min << "\t minimal value.\n";
-				cout << max << "\t maximal value.\n";
+				min = val;
+				max = val;
 			}
-			else if (a < min) {
-				min = a;
-				cout << min << "\t Minimal value.\n";
+			else if (val < min) {
+				min = val;
 			}
-			else if (a > max) {
-				max = a;
-				cout << max << "\t Maximal value.\n";
+			else if (val > max) {
+				max = val;
 			}
+			summ +=val;
 		}
 		else cout << "Not correct!\n";
 	}
+	cout << "You have been typed " << i << " times.\n";
+	cout << "The distance is " << summ << " meters.\n";
+	cout << min << "\t minimal value.\n";
+	cout << max << "\t maximal value.\n";
 	keep_window_open();
 }
