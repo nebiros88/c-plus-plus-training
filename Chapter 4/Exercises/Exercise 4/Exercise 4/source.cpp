428934752b9bfec80bc094054bbd4fe7b2486ef6
nebiros_88@mail.ru <nebiros_88@mail.ru>
2018-05-02 12:33:30 +0300

    **

--- Chapter 4/Exercises/Exercise 4/Exercise 4/source.cpp
+++ Chapter 4/Exercises/Exercise 4/Exercise 4/source.cpp
@@ -5,19 +5,37 @@ int main() {
 	string answer;
 	int n = 0;
 	int i = 0;
-	int d = (n0 + n1) / 2 ;
-	cout << "Please, make a number in case between 0 and 100. Is your number less than " << d << "?\n";
-	while (cin >> answer && i < 6) {
+	int d = (n0 + n1) / 2;
+	cout << "Please, make a number in case between 0 and 100 and if your are ready, type 'yes'.\n";
+	while (cin >> answer && i < 9) {
 		++i;
-		if (answer == "yes") {
-			n1 = d;
-			cout << "Is your number less than " << d / 2 << "?\n";
-			d = d / 2;
+		if (i <= 1) {
+			cout << "Is your number less than " << d  << "?\n";
 		}
-		else if (answer == "no") {
-			n0 = d;
-			cout << "Is your number less than " << (n0 + n1) / 2 << "?\n";
+		if (i > 1 && i != 7 && answer == "yes") {
+			n1 = d;
 			d = (n0 + n1) / 2;
+			cout << "Is your number less than " << d  << "?\n";
+			if (n1 == d) {
+				n0 = d;
+			}
+		}
+		else if (i > 1 && i != 7 && answer == "no") {
+			n0 = d;
+			d = (n0 + n1) / 2;
+			cout << "Is your number less than " << d << "?\n";
+			if (n0 == d) {
+				n1 = d;
+			}
+		}
+		if (i == 7 && answer == "yes") {
+			cout << "Your number is " << n0 << " !\n";
+		}
+		if (i == 7 && answer == "no") {
+			cout << "Your number is " << n1 - 1 << " ???\n";
+		}
+		if (i > 7 && answer == "no") {
+			cout << "Your number is " << d << " !!!\n";
 		}
 	}
 	keep_window_open();
