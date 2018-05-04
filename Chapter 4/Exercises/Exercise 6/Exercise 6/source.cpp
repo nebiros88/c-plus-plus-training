512cd72f27191f5b044ca81da025f27cfb720b8d
nebiros_88@mail.ru <nebiros_88@mail.ru>
2018-05-04 12:29:47 +0300

    new

--- Chapter 4/Exercises/Exercise 6/Exercise 6/source.cpp
+++ Chapter 4/Exercises/Exercise 6/Exercise 6/source.cpp
@@ -1,16 +1,22 @@
 # include "../../../../std_lib_facilities.h"
-string rename(string a) {
+int rename(string b) {
 	vector<string> a = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
-	for (int i = 0; i < a.size(); ++i);
-
+	for (int i = 0; i < a.size(); ++i) {
+		if (b == a[i]) {
+			return i;
+		}
+	}
+	return 10;
 }
 
 int main()
 {
-	string b = 0;
+	string b = " ";
 	cout << "Please make any number from 0 to 9  and press 'Enter'/\n";
 	while (cin >> b) {
-		cout << rename(b);
+		if (rename(b) == 10)
+		cout << "Not correct!\n";
+		else cout << rename(b);
 	}
 	keep_window_open();
 }
