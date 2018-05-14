bab661d4c03d5f326496c3728a3325aad99bc2df
nebiros_88@mail.ru <nebiros_88@mail.ru>
2018-05-14 14:42:09 +0300

    8

--- Chapter 4/Exercises/Exercise 8/Exercise 8/source.cpp
+++ Chapter 4/Exercises/Exercise 8/Exercise 8/source.cpp
@@ -1,9 +1,18 @@
 #include "../../../../std_lib_facilities.h"
+int is_calculate(double wish_grains) {
+	int i = 0;
+	while (wish_grains >= 1) {
+		++i;
+		wish_grains /= 2;
+	}
+	return i;
+}
 int main()
 {
-	int grains_number = 0;
+	double grains_number = 0;
 	int cell_number = 0;
-	int summ = 0;
+	double summ = 0;
+	double wish_grains = 0;
 	for (int i = 1; i <= 64; ++i) {
 		if (i == 1) {
 			grains_number = 1;
@@ -14,6 +23,9 @@ int main()
 		summ += grains_number;
 		cout << "Cell number is " << i << " and there are " << grains_number << " grains in it. You have " << summ << " grains at all.\n";
 	}
-
+	cout << "Please make number of grains your need and press 'Enter' and you will know how many cells you will need.\n";
+	while (cin >> wish_grains) {
+		cout << "The number of cell you will have to put your grains is " << is_calculate(wish_grains) << " cells.\n";
+	}
 keep_window_open();
 }
