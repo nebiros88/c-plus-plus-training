cb61f199c1d6763c879c89e0e6366b610d746452
nebiros_88@mail.ru <nebiros_88@mail.ru>
2018-05-22 11:59:14 +0300

    game

--- Chapter 4/Exercises/Exercise 10/Exercise 10/source.cpp
+++ Chapter 4/Exercises/Exercise 10/Exercise 10/source.cpp
@@ -4,37 +4,41 @@ int main()
 	char result = ' ';
 	char pc = 0;
 	char person = 0;
-	vector<char> human (10);
+	char human = ' ';
 	vector<char> ai = { 'r', 's', 'p', 'p', 's', 'r', 's', 's', 'p', 'r' };
-	cout << "Ok, let's play a game 'Rock - Scissors - Paper'. Press any ten values and press 'Enter'.\n";
-	for (char hum; cin >> hum;)
-	human.push_back(hum);
-	for (int i = 0; i < human.size(); ++i) {
-		for (int z = 0; z < ai.size(); ++z) {
-			if (ai[i] == human[i]) {
-				result = 0;
-			}
-			else if (ai[z] == 'r' || human[i] == 's' && ai[z] == 'p' || human[i] == 'r' && ai[z] == 's' || human[i] == 'p') {
-				result = 1;
-			}
-			else if (ai[z] == 's' || human[i] == 'r' && ai[z] == 'r' || human[i] == 'p' && ai[z] == 'p' || human[i] == 's') {
-				result = 2;
-			}
-			switch (result) {
-			case '0' :
-				pc += 1;
-				person += 1;
-				break;
-			case '1' :
-				pc += 1;
-				break;
-			case '2' :
-				person += 1;
-				break;
-			default :
-				cout << "Not correct!\n";
-			}
-			cout << "PC choise is " << ai[z] << " and your choise is " << human[i] << ".\n";
+	cout << "Ok, let's play a game 'Rock - Scissors - Paper'. Press any ten values and press 'Enter'.\n";;
+	for (int z = 0; z < ai.size(); ++z) {
+		cin >> human;
+		if (ai[z] == human) {
+			result = 0;
+		}
+		else if (ai[z] == 'r' || human == 's' && ai[z] == 'p' || human == 'r' && ai[z] == 's' || human == 'p') {
+			result = 1;
+		}
+		else if (ai[z] == 's' || human == 'r' && ai[z] == 'r' || human == 'p' && ai[z] == 'p' || human == 's') {
+			result = 2;
+		}
+		cout << "PC choise is " << ai[z] << " and your choise is " << human << ".\n";
+		if (result == 1) {
+			cout << "Loss.\n";
+		}
+		else if (result == 2) {
+			cout << "Victory.\n";
+		}
+		else if (result == 0) {
+			cout << "Draw.\n";
+		}
+		switch (result) {
+		case '0' :
+			pc += 1;
+			person += 1;
+			break;
+		case '1' :
+			pc += 1;
+			break;
+		case '2' :
+			person += 1;
+			break;
 		}
 	}
 	if (pc > person) {
