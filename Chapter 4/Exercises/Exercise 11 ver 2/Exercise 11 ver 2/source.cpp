46b1a59b9f0cd7b0ce09c4945554fa37453d6c2f
nebiros_88@mail.ru <nebiros_88@mail.ru>
2018-05-24 14:55:27 +0300

    besides_2

--- Chapter 4/Exercises/Exercise 11 ver 2/Exercise 11 ver 2/source.cpp
+++ Chapter 4/Exercises/Exercise 11 ver 2/Exercise 11 ver 2/source.cpp
@@ -2,9 +2,9 @@
 bool is_prime(int i) {
 	int val = i;
 	bool res = true;
-	for (int z = 1; z < sqrt(val); ++z) {
-		if (val % 2 == 0 || val % 3 == 0 || val % 5 == 0) {
-			res = false;
+	for (int z = 2; z < val; ++z) {
+		if (val % z  == 0) {
+			return false;
 		}
 		else res = true;
 	}
@@ -15,10 +15,12 @@ int main()
 	int result = 0;
 	vector<int> primes;
 	for (int i = 1; i < 100; ++i) {
-		if (is_prime(i) == true) {
-			result = i;
-			primes.push_back(result);
-			cout << "\t" << result << "\t";
+		if (i % 2 != 0) {
+			if (is_prime(i) == true) {
+				result = i;
+				primes.push_back(result);
+				cout << "\t" << result << "\t";
+			}
 		}
 	}
 	keep_window_open();
