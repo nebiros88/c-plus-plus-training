3e915aa9b5cce2dcb840b093347ba47792f74918
nebiros_88@mail.ru <nebiros_88@mail.ru>
2018-05-24 15:02:03 +0300

    12

---
+++ Chapter 4/Exercises/Exercise 12/Exercise 12/source.cpp
@@ -0,0 +1,30 @@
+#include "../../../../std_lib_facilities.h"
+bool is_prime(int i) {
+	int val = i;
+	bool res = true;
+	for (int z = 2; z < val; ++z) {
+		if (val % z == 0) {
+			return false;
+		}
+		else res = true;
+	}
+	return res;
+}
+int main()
+{
+	int result = 0;
+	vector<int> primes;
+	int max = 0;
+	cout << "Please, type maximal value and press 'Enter'.\n";
+	cin >> max;
+	for (int i = 1; i < max; ++i) {
+		if (i % 2 != 0) {
+			if (is_prime(i) == true) {
+				result = i;
+				primes.push_back(result);
+				cout << "\t" << result << "\t";
+			}
+		}
+	}
+	keep_window_open();
+}
