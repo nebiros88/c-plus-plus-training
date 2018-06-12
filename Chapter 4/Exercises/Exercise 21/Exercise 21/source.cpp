2bc580dd81c7009ad2ae52bf058b1afcec109a67
nebiros_88@mail.ru <nebiros_88@mail.ru>
2018-06-12 09:56:44 +0300

    21_last_one

---
+++ Chapter 4/Exercises/Exercise 21/Exercise 21/source.cpp
@@ -0,0 +1,50 @@
+#include "../../../../std_lib_facilities.h"
+int main()
+{
+	vector<string> names;
+	vector<double> scores;
+	string name;
+	double score = ' ';
+	int k = 0;
+	string x;
+	bool y = false;
+	int z = ' ';
+	cout << "Enter the name ant the age.\n";
+	cin >> name >> score;
+	while (name != "noname" && score != 0) {
+		names.push_back(name);
+		scores.push_back(score);
+		cin >> name >> score;
+	}
+	for (int i = 0; i < names.size(); ++i) {
+		for (int j = i + 1; j < names.size(); ++j) {
+			if (names[i] == names[j]) {
+				++k;
+			}
+		}
+	}
+	if (k > 0) {
+		cout << "There are similar words.\n";
+	}
+	cout << "Please enter the name and you will know their scores.\n ";
+	cin >> x;
+	for (int i = 0; i < names.size(); ++i) {
+		if (x == names[i]) {
+			k = i;
+			y = true;
+			cout << x << "\t" << scores[k] << "\n";
+		}
+	}
+	if (y == false) {
+		cout << "Can't find this name!\n";
+	}
+	cout << "Please, enter number of scores and you will see all peoples with with this csores.\n";
+	cin >> z;
+	for (int i = 0; i < scores.size(); ++i) {
+		if (z == scores[i]) {
+			cout << names[i] << "\t";
+		}
+	}
+	cout << "\n";
+	keep_window_open();
+}
