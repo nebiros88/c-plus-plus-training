2de14a035bcdce4c84fc54d5b426e60051eb142e
nebiros_88@mail.ru <nebiros_88@mail.ru>
2018-06-12 14:05:18 +0300

    last_21

--- Chapter 4/Exercises/Exercise 21/Exercise 21/source.cpp
+++ Chapter 4/Exercises/Exercise 21/Exercise 21/source.cpp
@@ -38,13 +38,17 @@ int main()
 	if (y == false) {
 		cout << "Can't find this name!\n";
 	}
+	y = false;
 	cout << "Please, enter number of scores and you will see all peoples with with this csores.\n";
 	cin >> z;
 	for (int i = 0; i < scores.size(); ++i) {
 		if (z == scores[i]) {
+			y = true;
 			cout << names[i] << "\t";
 		}
 	}
-	cout << "\n";
+	if (y == false) {
+		cout << "Check your scores!\n";
+	}
 	keep_window_open();
 }
