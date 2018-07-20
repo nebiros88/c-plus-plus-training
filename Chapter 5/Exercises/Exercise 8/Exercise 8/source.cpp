5b81dd7a075603a437857725f34c2fdc68a19f06
nebiros_88@mail.ru <nebiros_88@mail.ru>
2018-07-20 15:37:32 +0300

    the last

---
+++ Chapter 5/Exercises/Exercise 8/Exercise 8/source.cpp
@@ -0,0 +1,23 @@
+#include "../../../../std_lib_facilities.h"
+int main()
+{
+	int n = 0;
+	vector<int> integers;
+	int summ = 0;
+	cout << "Enter the number of summable values and press 'Enter'.\n";
+	cin >> n;
+	cout << "Enter any integers (| - for ending input).\n";
+	for (int val; cin >> val;) {
+		integers.push_back(val);
+	}
+	for (int i = 0; i < n; ++i) {
+		summ += integers[i];
+	}
+	cout << "Summ of first " << n << " values (";
+	for (int i = 0; i < n; ++i) {
+		cout << integers[i] << " , ";
+	}
+	cout << ") is " << summ << '\n';
+	keep_window_open();
+	keep_window_open();
+}
