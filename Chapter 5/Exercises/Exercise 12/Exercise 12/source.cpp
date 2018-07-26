963b3bb94402fa3a87bc56da0d9440416e1f9963
nebiros_88@mail.ru <nebiros_88@mail.ru>
2018-07-26 14:53:17 +0300

    remake for mistakes_search

--- Chapter 5/Exercises/Exercise 12/Exercise 12/source.cpp
+++ Chapter 5/Exercises/Exercise 12/Exercise 12/source.cpp
@@ -4,30 +4,39 @@ int main()
 	vector<int> values = { 1, 2, 3, 4 };
 	bool result = false;
 	cout << "Please enter any 4 values form 0 to 9 and press 'Enter'.\n";
-	while (result != true) {
-		vector<int> answers;
-		int bools = 0;
-		int cows = 0;
-		int answer = 0;
-		for (int i = 0; i < 4; ++i) {
-			cin >> answer;
-			answers.push_back(answer);
-		}
-		for (int i = 0; i < values.size(); ++i) {
-			for (int j = 0; j < values.size(); ++j) {
-				if (answers[j] == values[i] && i == j) {
-					++bools;
-				}
-				else if (answers[j] == values[i]) {
-					++cows;
+	try {
+		while (result != true) {
+
+			vector<int> answers;
+			int bools = 0;
+			int cows = 0;
+			int answer = 0;
+			for (int i = 0; i < 4; ++i) {
+				cin >> answer;
+				if (answer < 0 || answer > 9) error("Not correct values!\n");
+				answers.push_back(answer);
+			}
+			for (int i = 0; i < values.size(); ++i) {
+				for (int j = 0; j < values.size(); ++j) {
+					if (answers[j] == values[i] && i == j) {
+						++bools;
+					}
+					else if (answers[j] == values[i]) {
+						++cows;
+					}
 				}
 			}
+			cout << bools << " bools " << cows << " cows\n";
+			if (bools == 4) {
+				result = true;
+			}
 		}
-		cout << bools << " bools " << cows << " cows\n";
-		if (bools == 4) {
-			result = true;
-		}
+		cout << "Your win!\n";
+		keep_window_open();
+		keep_window_open();
 	}
-	cout << "Your win!\n";
-	keep_window_open();
+	catch (runtime_error & e) {
+		cerr << e.what();
+		keep_window_open();
+	}
 }
