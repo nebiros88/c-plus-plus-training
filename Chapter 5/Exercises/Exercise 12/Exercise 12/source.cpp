9090c0a480fe1ec41ee402f4fae21f5d51875fe2
nebiros_88@mail.ru <nebiros_88@mail.ru>
2018-07-26 11:24:13 +0300

    Bools&Cows

---
+++ Chapter 5/Exercises/Exercise 12/Exercise 12/source.cpp
@@ -0,0 +1,33 @@
+#include "../../../../std_lib_facilities.h"
+int main()
+{
+	vector<int> values = { 1, 2, 3, 4 };
+	bool result = false;
+	cout << "Please enter any 4 values form 0 to 9 and press 'Enter'.\n";
+	while (result != true) {
+		vector<int> answers;
+		int bools = 0;
+		int cows = 0;
+		int answer = 0;
+		for (int i = 0; i < 4; ++i) {
+			cin >> answer;
+			answers.push_back(answer);
+		}
+		for (int i = 0; i < values.size(); ++i) {
+			for (int j = 0; j < values.size(); ++j) {
+				if (answers[j] == values[i] && i == j) {
+					++bools;
+				}
+				else if (answers[j] == values[i]) {
+					++cows;
+				}
+			}
+		}
+		cout << bools << " bools " << cows << " cows\n";
+		if (bools == 4) {
+			result = true;
+		}
+	}
+	cout << "Your win!\n";
+	keep_window_open();
+}
