ed3a15251233c0098067d04b56a55e5814351fb8
nebiros_88@mail.ru <nebiros_88@mail.ru>
2018-08-02 16:28:49 +0300

    remake14

--- Chapter 5/Exercises/Exercise14/Exercise14/source.cpp
+++ Chapter 5/Exercises/Exercise14/Exercise14/source.cpp
@@ -1,19 +1,22 @@
 #include "../../../../std_lib_facilities.h"
-bool are_the_same(string day)
+bool is_weekday(string day)
 {
 	vector<string> day_names = { "monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday", "mon", "tue", "wed", "thu", "fri", "sat", "sun" };
 	string val = day;
-	int res = 0;
 	for (int i = 0; i < day_names.size(); ++i) {
 		if (val == day_names[i]) {
-			++res;
+			return true;
 		}
 	}
-	if (res > 0) {
-		return true;
+	return false;
+}
+int summ(vector<int>day)
+{
+	int summ = 0;
+	for (int i = 0; i < day.size(); ++i) {
+		summ += day[i];
 	}
-	else return false;
-
+	return summ;
 }
 int main()
 {
@@ -24,16 +27,9 @@ int main()
 	vector<int> friday;
 	vector<int> saturday;
 	vector<int> sunday;
-	int summ_monday = 0;
-	int summ_tuesday = 0;
-	int summ_wednesday = 0;
-	int summ_thursday = 0;
-	int summ_friday = 0;
-	int summ_saturday = 0;
-	int summ_sunday = 0;
-	int summ_errors = 0;
 	string day = "..";
 	int val = 0;
+	int summ_errors = 0;
 	cout << "Enter the day of the week and any value.\n";
 	while (cin >> day >> val) {
 		if (day == "monday" || day == "mon") {
@@ -57,7 +53,7 @@ int main()
 		if (day == "sunday" || day == "sun") {
 			sunday.push_back(val);
 		}
-		if (!are_the_same(day)) {
+		if (is_weekday(day)== false) {
 			++summ_errors;
 			cout << "Incorrect day name!\n";
 		}
@@ -65,45 +61,38 @@ int main()
 	cout << "Monday (";
 	for (int i = 0; i < monday.size(); ++i) {
 		cout << monday[i] << '\t';
-		summ_monday += monday[i];
 	}
-	cout << ") Summ is " << summ_monday << '\n';
+	cout << ") Summ is " << summ(monday) << '\n';
 	cout << "Tuesday (";
 	for (int i = 0; i < tuesday.size(); ++i) {
 		cout << tuesday[i] << '\t';
-		summ_tuesday += tuesday[i];
 	}
-	cout << ") Summ is " << summ_tuesday << '\n';
+	cout << ") Summ is " << summ(tuesday) << '\n';
 	cout << "Wednesday (";
 	for (int i = 0; i < wednesday.size(); ++i) {
 		cout << wednesday[i] << '\t';
-		summ_wednesday += wednesday[i];
 	}
-	cout << ") Summ is " << summ_wednesday << '\n';
+	cout << ") Summ is " << summ(wednesday) << '\n';
 	cout << "Thursday (";
 	for (int i = 0; i < thursday.size(); ++i) {
 		cout << thursday[i] << '\t';
-		summ_thursday += wednesday[i];
 	}
-	cout << ") Summ is " << summ_thursday << '\n';
+	cout << ") Summ is " << summ(thursday) << '\n';
 	cout << "Friday (";
 	for (int i = 0; i < friday.size(); ++i) {
 		cout << friday[i] << '\t';
-		summ_friday += friday[i];
 	}
-	cout << ") Summ is " << summ_friday << '\n';
+	cout << ") Summ is " << summ(friday) << '\n';
 	cout << "Saturday (";
 	for (int i = 0; i < saturday.size(); ++i) {
 		cout << saturday[i] << '\t';
-		summ_saturday += saturday[i];
 	}
-	cout << ") Summ is " << summ_saturday << '\n';
+	cout << ") Summ is " << summ(saturday) << '\n';
 	cout << "Sunday (";
 	for (int i = 0; i < sunday.size(); ++i) {
 		cout << sunday[i] << '\t';
-		summ_sunday += sunday[i];
 	}
-	cout << ") Summ is " << summ_sunday << '\n';
+	cout << ") Summ is " << summ(sunday) << '\n';
 	cout << "You enter " << summ_errors << " incorrect names of days!\n";
 	keep_window_open();
 	keep_window_open();
